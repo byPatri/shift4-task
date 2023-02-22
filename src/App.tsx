293dea4832c1f354b71f0beaaa64f30d8a346c44
byPatri <patrycja.krzeszowiec@gmail.com>
2023-02-22 17:51:18 +0100

    initial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.scss'
+
+function App() {
+
+  return (
+    <div className="app">
+      <div className="header"></div>
+      <button>Cancel</button>
+      <button>Continue</button>
+    </div>
+  )
+}
+
+export default App
