21d156a2fede944ee8685e776918c81195b6fff4
byPatri <patrycja.krzeszowiec@gmail.com>
2023-02-23 21:10:10 +0100

    close button, assets fix

--- src/components/cardHeader/index.tsx
+++ src/components/cardHeader/index.tsx
@@ -1,9 +1,15 @@
-import logo from '../../assets/giving-icon.png';
+import logo from '../../assets/giving_icon.svg';
+import close from '../../assets/close_btn.svg';
 import './index.scss';
 
 function CartHeader(){
     return (
         <div className='card-header'>
+            <img
+                src={close}
+                className="close-button"
+                alt="close button"
+            />
             <img src={logo} alt="card-logo" />
             <span className='card-header_title'>The giving block</span>
             <span className='card-header_subtitle'>Set up your donation goal!</span>
